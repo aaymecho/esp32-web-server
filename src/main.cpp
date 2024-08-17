f8c6efdf70e90c9ff7f1637ba496bcfc6c117c9c
slim <aaymecho3@gatech.edu>
2024-08-16 22:21:34 -0400

    updated code for LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 
-const int ledPin = 4;
+const int ledPin1 = 4;
+const int ledPin2 = 5;
 const unsigned long interval = 1000;
 unsigned long previousMillis = 0;
-bool ledState = LOW;
+bool ledState1 = LOW;
+bool ledState2 = LOW;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
@@ -14,7 +17,11 @@ void loop() {
 
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    ledState = !ledState;
-    digitalWrite(ledPin, ledState);
+
+    ledState1 = !ledState1;
+    ledState2 = !ledState2;
+
+    digitalWrite(ledPin1, ledState1);
+    digitalWrite(ledPin2, ledState2);
   }
 }
